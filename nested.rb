e19bf6e86effa47abbf763652c89bd10bfb009a7
Hugo Rodrigez <hugor37@gmail.com>
2020-08-06 20:49:28 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -65,13 +65,7 @@ programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
-	programmer_hash[0] =
-	  {
-		 :yukihiro_matsumoto => {
-	   :known_for => "Ruby",
-	   :languages => ["LISP", "C"]
-     }
-	 }
+
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
 #   :known_for => "Ruby",
